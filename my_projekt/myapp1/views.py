5419d2880155a343a24e1e5265519cfe99b5e766
NikolayGlazkov <gn9377419582@gmail.com>
2023-06-26 15:56:10 +0300

    make_index_array_of_worker

--- my_projekt/myapp1/views.py
+++ my_projekt/myapp1/views.py
@@ -3,13 +3,8 @@ from myapp1.models import Worker
 
 def index_page(request):
     
-    
+    my_dict = {"data":"приветик","data2":"как дела?"}
 
     all_workers = Worker.objects.all()
-    print(all_workers)
-    Worker_filtered = Worker.objects.filter(salary=60000)
-    print(Worker_filtered)
-
-    for i in all_workers:
-        print(f"№{i.id}) Имя сотрудника: {i.name}, Фамилия: {i.second_name}, Зарплата: {i.salary}")
-    return render(request,'index.html')
+   
+    return render(request,'index.html',{"data":all_workers})
