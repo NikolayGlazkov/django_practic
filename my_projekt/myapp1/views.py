a3992bdacbdcd9900128115b777f010162a813fe
NikolayGlazkov <gn9377419582@gmail.com>
2023-06-26 13:25:32 +0300

    make_som_backend

--- my_projekt/myapp1/views.py
+++ my_projekt/myapp1/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from myapp1.models import Worker
 
 def index_page(request):
+    
+    
+
+    all_workers = Worker.objects.all()
+    print(all_workers)
+    Worker_filtered = Worker.objects.filter(salary=60000)
+    print(Worker_filtered)
+
+    for i in all_workers:
+        print(f"№{i.id}) Имя сотрудника: {i.name}, Фамилия: {i.second_name}, Зарплата: {i.salary}")
     return render(request,'index.html')
